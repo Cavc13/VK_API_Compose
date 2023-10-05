b5d31585e8757579245d2d8cfeca3eb4c4bbca27
Konstantin Mikhailov <k.mikhailov@3205.team>
2023-10-05 20:02:26 +0300

    Add Flow in MainViewModel

--- app/src/main/java/com/snusnu/vkapicompose/presentation/main/MainActivity.kt
+++ app/src/main/java/com/snusnu/vkapicompose/presentation/main/MainActivity.kt
@@ -6,8 +6,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.snusnu.vkapicompose.domain.AuthState
 import com.snusnu.vkapicompose.ui.theme.VKAPIComposeTheme
 import com.vk.api.sdk.VK
 import com.vk.api.sdk.auth.VKScope
@@ -20,12 +21,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             VKAPIComposeTheme {
                 val viewModel: MainViewModel = viewModel()
-                val authState = viewModel.authState.observeAsState(AuthState.Initial)
+                val authState = viewModel.authState.collectAsState(AuthState.Initial)
 
                 val launcher = rememberLauncherForActivityResult(
                     contract = VK.getVKAuthActivityResultContract()
                 ) {
-                    viewModel.performAuthResult(it)
+                    viewModel.performAuthResult()
                 }
 
                 when(authState.value) {
